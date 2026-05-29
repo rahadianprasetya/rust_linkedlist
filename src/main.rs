554dcddfdf12b5519dbf318aaa87c2081a7faca4
rahadian.prast <rahadian.prast@gmail.com>
2026-05-29 22:34:45 +0700

    add Rust Double Linkedlist

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+// Refcell, Immutable outside, but can mutate interior
+// Rc, Reference counting pointer
+use std::{cell::RefCell, rc::Rc, rc::Weak};
+
 fn main() {
     println!("Hello, world!");
 }
@@ -29,9 +33,98 @@ impl<T> LinkedList<T> {
     }
 }
 
+/*
+ * Double LinkedList
+ */
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct DbNode<T> {
+    data: T,
+    next: Option<Rc<RefCell<DbNode<T>>>>,
+    prev: Option<Weak<RefCell<DbNode<T>>>>,
+}
+
+#[derive(Debug)]
+pub struct DbList<T> {
+    first: Option<Rc<RefCell<DbNode<T>>>>,
+    last: Option<Weak<RefCell<DbNode<T>>>>,
+}
+
+impl<T> Default for DbList<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> DbList<T> {
+    pub fn new() -> Self {
+        DbList {
+            first: None,
+            last: None,
+        }
+    }
+
+    pub fn push_front(&mut self, data: T) {
+        match self.first.take() {
+            Some(r) => {
+                // create new front_object
+                let new_front = Rc::new(RefCell::new(DbNode {
+                    data,
+                    next: Some(r.clone()),
+                    prev: None,
+                }));
+                // tell the object now this is now in front of it
+                let mut m = r.borrow_mut();
+                m.prev = Some(Rc::downgrade(&new_front));
+                // put this on the front
+                self.first = Some(new_front);
+            }
+            None => {
+                let new_data = Rc::new(RefCell::new(DbNode {
+                    data,
+                    next: None,
+                    prev: None,
+                }));
+                self.last = Some(Rc::downgrade(&new_data));
+                self.first = Some(new_data);
+            }
+        }
+    }
+
+    pub fn push_back(&mut self, data: T) {
+        match self.last.take() {
+            Some(r) => {
+                // create new back object
+                let new_back = Rc::new(RefCell::new(DbNode {
+                    data,
+                    prev: Some(r.clone()),
+                    next: None,
+                }));
+                // tell the object now this is now in behind of it
+                let st = Weak::upgrade(&r).unwrap();
+                let mut m = st.borrow_mut();
+                self.last = Some(Rc::downgrade(&new_back));
+                m.next = Some(new_back);
+                // put this on the front
+            }
+            None => {
+                let new_data = Rc::new(RefCell::new(DbNode {
+                    data,
+                    next: None,
+                    prev: None,
+                }));
+                self.last = Some(Rc::downgrade(&new_data));
+                self.first = Some(new_data);
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::collections::LinkedList;
+
+    use crate::DbList;
 
     #[test]
     fn test_linkedlist() {
@@ -41,5 +134,14 @@ mod tests {
         ll.push_front(1);
 
         println!("ll {:?}", ll);
+    }
+
+    #[test]
+    fn test_dblist() {
+        let mut dl = DbList::new();
+        dl.push_front(6);
+        dl.push_back(11);
+        dl.push_front(5);
+        println!("DbList {:?}", dl);
     }
 }
