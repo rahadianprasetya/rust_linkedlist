8ba20cb6a25b399103825fee27f78f6a67392d14
rahadian.prast <rahadian.prast@gmail.com>
2026-05-29 11:37:21 +0700

    add Rust Linkedlist

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[derive(Debug)]
+pub struct LinkedList<T>(Option<(T, Box<LinkedList<T>>)>);
+
+impl<T> Default for LinkedList<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        LinkedList(None)
+    }
+
+    pub fn push_front(&mut self, data: T) {
+        let t = self.0.take();
+        self.0 = Some((data, Box::new(LinkedList(t))));
+    }
+
+    pub fn push_back(&mut self, data: T) {
+        match self.0 {
+            Some((_, ref mut child)) => child.push_back(data),
+            None => self.push_front(data),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::LinkedList;
+
+    #[test]
+    fn test_linkedlist() {
+        let mut ll = LinkedList::new();
+        ll.push_front(3);
+        ll.push_back(12);
+        ll.push_front(1);
+
+        println!("ll {:?}", ll);
+    }
+}
