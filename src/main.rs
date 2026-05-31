7e2b85e1519358de307478e2aba92754048903e8
rahadian.prast <rahadian.prast@gmail.com>
2026-05-31 21:47:18 +0700

    add managing heights of skippList

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use std::{
     cell::RefCell,
     cmp::max,
-    fmt::Debug,
+    fmt::{Debug, Display, Write},
     rc::{Rc, Weak},
 };
 
@@ -263,11 +263,170 @@ impl<T: Debug> BinTree<T> {
     }
 }
 
+// Skipp List
+
+type Rcc<T> = Rc<RefCell<T>>;
+
+pub fn rcc<T>(t: T) -> Rcc<T> {
+    Rc::new(RefCell::new(t))
+}
+
+#[derive(Debug)]
+pub struct SkipNode<T: PartialOrd> {
+    right: Option<Rcc<SkipNode<T>>>,
+    down: Option<Rcc<SkipNode<T>>>,
+    data: Rcc<T>,
+}
+
+#[derive(Debug)]
+pub struct SkipList<T: PartialOrd>(Vec<SkipNode<T>>);
+
+impl<T: PartialOrd> SkipNode<T> {
+    pub fn new(t: T) -> Self {
+        SkipNode {
+            right: None,
+            down: None,
+            data: rcc(t),
+        }
+    }
+
+    pub fn insert(&mut self, dt: T) -> Option<Rcc<SkipNode<T>>> {
+        // bigger than right -> go right
+        if let Some(ref mut rt) = self.right {
+            if dt > *rt.borrow().data.borrow() {
+                return rt.borrow_mut().insert(dt);
+            }
+        }
+
+        // try lower layer
+        if let Some(ref dw) = self.down {
+            return match dw.borrow_mut().insert(dt) {
+                Some(child) => match rand::random::<bool>() {
+                    true => {
+                        let dt = child.borrow().data.clone();
+
+                        let nn = SkipNode {
+                            right: self.right.take(),
+                            data: dt,
+                            down: Some(child),
+                        };
+
+                        let res = rcc(nn);
+
+                        self.right = Some(res.clone());
+
+                        Some(res)
+                    }
+
+                    false => None,
+                },
+
+                None => None,
+            };
+        }
+
+        // bottom level insertion
+        let mut nn = SkipNode::new(dt);
+
+        nn.right = self.right.take();
+
+        let res = rcc(nn);
+
+        self.right = Some(res.clone());
+
+        Some(res)
+    }
+}
+
+impl<T: PartialOrd> Default for SkipList<T> {
+    fn default() -> Self {
+        Self(Default::default())
+    }
+}
+
+impl<T: PartialOrd> SkipList<T> {
+    pub fn new() -> Self {
+        SkipList(Vec::new())
+    }
+
+    pub fn insert(&mut self, data: T) {
+        if self.0.is_empty() {
+            self.0.push(SkipNode::new(data));
+            return;
+        }
+        // our vec will have the lowest row, with the lowes number
+        for i in (0..self.0.len()).rev() {
+            if data > *self.0[i].data.borrow() {
+                if let Some(ch) = self.0[i].insert(data) {
+                    // TODO loop up on 50:50 chance
+                    self.loop_up(ch, i + 1);
+                }
+                return;
+            }
+        }
+        // if none those succeeded, that means we have an element to replace the first
+        let mut nn = SkipNode::new(data);
+        // put our new element on the front of the row
+        std::mem::swap(&mut nn, &mut self.0[0]);
+        let res = rcc(nn);
+        self.0[0].right = Some(res.clone());
+        self.loop_up(res, 1);
+        // TODO loop up on 50:50 chance
+    }
+
+    pub fn loop_up(&mut self, ch: Rcc<SkipNode<T>>, n: usize) {
+        if rand::random::<bool>() == true {
+            return;
+        }
+        let dt = ch.borrow().data.clone();
+
+        let mut nn = SkipNode {
+            right: None,
+            down: Some(ch),
+            data: dt,
+        };
+
+        if n >= self.0.len() {
+            self.0.push(nn);
+            return;
+        }
+
+        std::mem::swap(&mut nn, &mut self.0[n]);
+        let res = rcc(nn);
+        self.0[n].right = Some(res.clone());
+        self.loop_up(res, n + 1);
+    }
+}
+
+impl<T: Debug + PartialOrd> SkipNode<T> {
+    pub fn print_row<W: Write>(&self, w: &mut W) -> std::fmt::Result {
+        write!(w, ",{:?}", self.data.borrow())?;
+        if let Some(ref r) = self.right {
+            r.borrow().print_row(w)?;
+        }
+        Ok(())
+    }
+}
+
+impl<T: Debug + PartialOrd> Display for SkipList<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.0.is_empty() {
+            return write!(f, "SkipList<Empty>");
+        }
+        for sn in &self.0 {
+            //write!(f, "\n")?;
+            writeln!(f)?;
+            sn.print_row(f)?;
+        }
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::collections::LinkedList;
 
-    use crate::{BinTree, DbList};
+    use crate::{BinTree, DbList, SkipList, SkipNode};
 
     #[test]
     fn test_linkedlist() {
@@ -330,5 +489,27 @@ mod tests {
             t.add_sorted(i);
         }
         t.print_lfirst(0);
+    }
+
+    #[test]
+    fn test_skipp_node() {
+        let mut snode = SkipNode::new(4);
+        snode.insert(4);
+        snode.insert(6);
+        snode.insert(77);
+        snode.insert(88);
+        snode.insert(23);
+        println!("s-{:?}", snode);
+    }
+
+    #[test]
+    fn test_skipp_list() {
+        let mut s = SkipList::new();
+        s.insert(4);
+        s.insert(6);
+        s.insert(77);
+        s.insert(88);
+        s.insert(23);
+        println!("s={}", s);
     }
 }
